12af2bb3921700a73767f1cd03ee51367f9426cb
dharmendra singh <dharmendrasingh@msn.com>
2018-03-20 19:25:32 -0400

    Added new tests.

---
+++ src/test/java/org/homedepot/SwaggerConfigTest.java
@@ -0,0 +1,8 @@
+package org.homedepot;
+
+/**
+ * Created by dharma on 3/20/18.
+ */
+public class SwaggerConfigTest {
+
+}
---
+++ src/test/java/org/homedepot/service/StoreRetreiveServiceTest.java
@@ -0,0 +1,33 @@
+package org.homedepot.service;
+
+import org.junit.Test;
+
+/**
+ * Created by dharma on 3/20/18.
+ */
+public class StoreRetreiveServiceTest {
+    @Test
+    public void listAllVarValue() throws Exception {
+    }
+
+    @Test
+    public void retreiveByVarName() throws Exception {
+    }
+
+    @Test
+    public void varExits() throws Exception {
+    }
+
+    @Test
+    public void saveVarValue() throws Exception {
+    }
+
+    @Test
+    public void deleteAll() throws Exception {
+    }
+
+    @Test
+    public void updateVarValue() throws Exception {
+    }
+
+}
