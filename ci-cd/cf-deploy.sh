b9b04a70538b0951edf105b40c3bd03e22077f40
dharmendra singh <dharmendrasingh@msn.com>
2018-03-22 14:10:59 -0400

    Adding the deployment scripts.

--- ci-cd/cf-deploy.sh
+++ ci-cd/cf-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary\" | tar -zx
+curl -L \"https://cli.run.pivotal.io/stable?release=linux64-binary\" | tar -zx
 ./cf --version
 ./cf login -a https://api.run.pivotal.io -u dharmendra.singh4@cognizant.com -p Hr32*sapient -s development
 ./cf push
