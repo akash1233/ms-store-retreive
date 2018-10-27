757f92ebd6be9cf02b310168bfd44b9fdba27b70
dharmendra singh <dharmendrasingh@msn.com>
2018-10-27 17:08:34 -0400

    fixed the tests.

--- src/test/java/org/cts/integrationtests/RestAssuredIT.java
+++ src/test/java/org/cts/integrationtests/RestAssuredIT.java
@@ -18,7 +18,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 public class RestAssuredIT {
     @Before
     public void setUpTest() {
-        RestAssured.baseURI = "https://store-retreive.cfapps.io/api";
+//        RestAssured.baseURI = "https://store-retreive.cfapps.io/api";
     }
 
     @Test
@@ -26,13 +26,13 @@ public class RestAssuredIT {
         RequestSpecification httpRequest = RestAssured.given();
         // Make a GET request call directly by using RequestSpecification.get() method.
         // Make sure you specify the resource name.
-        Response response = httpRequest.get("/retrivestoredval/");
+        Response response = httpRequest.get("https://store-retreive.cfapps.io/api/retrivestoredval/");
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200);
     }
 
     @Test
     public void testRetriveStoredValTestDataValidation() {
-        get("/retrivestoredval/").then().body("varname", equalTo(Arrays.asList("xyz")));
+        get("https://store-retreive.cfapps.io/api/retrivestoredval/").then().body("varname", equalTo(Arrays.asList("xyz")));
     }
 }
