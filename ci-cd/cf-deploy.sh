993fa459b5ad72e08554f1f5a3d04e338e018b95
dharmendra singh <dharmendrasingh@msn.com>
2018-10-27 14:16:06 -0400

    updated the pip file and added new tests.

--- ci-cd/cf-deploy.sh
+++ ci-cd/cf-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
-#curl -L \"https://cli.run.pivotal.io/stable?release=linux64-binary\" | tar -zx
+curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
+mv cf /usr/local/bin
 ./cf --version
 ./cf login -a https://api.run.pivotal.io -u dharmendra.singh4@cognizant.com -p Hr32*sapient -s development
 ./cf push
