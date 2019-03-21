3f9c53d85b4f80f9029126313192550dac35c602
dharmendra singh <492408@cognizant.com>
2019-03-21 18:32:18 -0400

    changed the pivotal passwords.

--- ci-cd/cf-deploy.sh
+++ ci-cd/cf-deploy.sh
@@ -3,5 +3,5 @@
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 mv cf /usr/local/bin
 ./cf --version
-./cf login -a https://api.run.pivotal.io -u akku12345@gmail.com -p Hr32*sapient -s development
+./cf login -a https://api.run.pivotal.io -u akku.util@gmail.com -p Hr32*sapient -s development
 ./cf push
