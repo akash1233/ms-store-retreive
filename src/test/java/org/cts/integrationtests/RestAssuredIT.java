daa0a1b954a896488e1bec9d4d701bf87396395e
Aditya Mandlekar <aditya.mandlekar@thomsonreuters.com>
2019-06-26 18:50:47 -0400

    Test Case Refined

--- src/test/java/org/cts/integrationtests/RestAssuredIT.java
+++ src/test/java/org/cts/integrationtests/RestAssuredIT.java
@@ -1,272 +1,219 @@
 package org.cts.integrationtests;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-
-import org.cts.controller.SRController;
-import org.hamcrest.Matcher;
+import org.cts.model.StoredValue;
+import org.cts.service.StoreRetreiveService;
+import org.hamcrest.Matchers;
+import org.json.JSONException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import static io.restassured.RestAssured.*;
 import io.restassured.http.ContentType;
-
-import static org.hamcrest.Matchers.*;
-
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import static io.restassured.RestAssured.get;
-import static org.hamcrest.CoreMatchers.equalTo;
+
 
 /**
  * Created by dharma on 5/28/18.
- * updated by Aditya on 04/08/19.
+ * updated by Adi on 06/08/19.
  */
 
-public class RestAssuredIT {
+public abstract class RestAssuredIT implements StoreRetreiveService {
 	  public static final org.slf4j.Logger logger = LoggerFactory.getLogger(RestAssuredIT.class);
+	  private List<StoredValue> storedValues;
 	  
     @Before
     public void setUpTest() {
-        //RestAssured.baseURI = "https://store-retreive.cfapps.io/api";
-    	RestAssured.baseURI = "http://localhost:8080/api";
+    	 RestAssured.baseURI = "http://localhost:8080/api";
     }
-    
-    //Work in Progress to fix the Test Case failures and Exceptions
-  
-    //1. GET
+     
+    //1. listAllVarValue - Negative
     @Test
-    public void testRetriveStoredValUp() {
+    public void testlistAllVarValue_negative() {
     	try {
         RequestSpecification httpRequest = RestAssured.given();
-        // Make a GET request call directly by using RequestSpecification.get() method.
-        // Make sure you specify the resource name.
-        Response response = httpRequest.get("/retrivestoredval/");
-        int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 204);
+        Response response = httpRequest.accept(ContentType.JSON).get("/retrivestoredval/");
+        Assert.assertThat(response.getStatusCode(), Matchers.equalTo(204));
+        logger.info("RestAssuredIT::"+"Method Name :: testRetriveStoredValUp::"+"testlistAllVarValue"+ "HttpStatus.NO_CONTENT(204)::"+ "Validation is Succesful");
     	}
     	catch (Exception e)
     	{
-    		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testRetriveStoredValUp::"+ e.getMessage());
-    	}
-    }
-
-    @Test
-    public void testRetriveStoredValTestDataValidation() {
-    	try {
-    	 String value="string";
-    	 int statusCode =get("/retrivestoredval/").then().body("value", equalTo(Arrays.asList(value))).statusCode(200).extract().response().statusCode();
-    	  Assert.assertEquals(statusCode, 204);
-    	} 
-    	  catch (Exception e)
-      	{
-      		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testRetriveStoredValTestDataValidation::"+ e.getMessage());
-      	}
-    }
-    
-    @Test
-    public void testRetriveStoredValTestDataValidation_badauthentication() {
-    	try {
-    	Map<String,String> variableValues = new HashMap<>();
-    	variableValues.put("variable1", "value1");
-    	variableValues.put("variable2", "value2");
-    	int statusCode = given().auth().basic("badusername","badpassword").get("/retrivestoredval/").then().statusCode(404).extract().response().statusCode();
-    	Assert.assertEquals(statusCode, 404);
-    	}
-    	catch (Exception e)
-    	{
-    		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testRetriveStoredValTestDataValidation_badauthentication::"+ e.getMessage());
-    	}
-    }
-    
-    @Test
-    public void testRetriveStoredValTestDataValidation_negative() {
-    	try {
-    	Map<String,String> variableValues = new HashMap<>();
-    	variableValues.put("variable1", "value1");
-    	variableValues.put("variable2", "value2");
-    	int statusCode = get("/retrivestoredval/").then().body("variable1", equalTo(Arrays.asList("test"))).statusCode(404).extract().response().statusCode();
-    	Assert.assertEquals(statusCode, 404);
-    	}
-    	catch (Exception e)
-    	{
-    		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testRetriveStoredValTestDataValidation_variable::"+ e.getMessage());
-    	}
-    }
-    
-    @Test
-    public void testRetriveStoredValTestDataValidation_variable() {
-    	try {
-    	String variable = "test";
-        given().pathParam("variablename",variable).when().get("/retrivestoredval/{variablename}").then().assertThat().body("variablename",hasSize(1));
-    	}
-        catch (Exception e)
-    	{
-    		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testRetriveStoredValTestDataValidation_variable::"+ e.getMessage());
-    	}
-    }
-    
-    @Test
-    public void testRetriveStoredValTestDataValidation_notfound() {
-    	try {
-    	String variable = "";
-    	int statusCode=given().pathParam("variablename",variable).when().get("/retrivestoredval/{variablename}").then().statusCode(404).extract().response().statusCode();
-        Assert.assertEquals(statusCode, 404);
-    	}
-        catch (Exception e)
-    	{
-    		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testRetriveStoredValTestDataValidation_notfound::"+ e.getMessage());
-    	}
-    }
-    
-    @Test
-    public void testRetriveStoredVallistall() {
-    	try {
-    	Map<String,String> variableValues = new HashMap<>();
-    	variableValues.put("variable1", "value1");
-    	variableValues.put("variable2", "value2");
-    	variableValues.put("variable3", "value3");
+    		logger.error("Error in Class - "+"RestAssuredIT::"+"Method Name :: testlistAllVarValue_negative::"+ e.getMessage());
     	
-    	 given().contentType("application/json").body(variableValues).when().get("/retrivestoredval/").then()
-    	.body("variable1", equalTo("value1"))
-        .body("variable2", equalTo("value2"))
-    	.body("variable3", equalTo("value3"));
     	}
-    	 catch (Exception e)
-     	{
-     		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testRetriveStoredVallistall::"+ e.getMessage());
-     	}
     }
     
-    //POST
+    //2. listAllVarValue
     @Test
-    public void testcreateVarValue_positive() {
+    public void testlistAllVarValue() {
     	try {
-        Map<String,String> createvariable = new HashMap<>();
-        createvariable.put("storedValue", "xyx1111");
-        int statusCode=given().contentType("application/json").body(createvariable).when().post("/api/storevalue/").then().statusCode(201).extract().response().statusCode();
-        Assert.assertEquals(statusCode, 201);
+    		
+    	storedValues = new ArrayList<StoredValue>();
+        StoredValue first = new StoredValue("xyz", "2345");
+        this.saveVarValue(first);
+             
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.accept(ContentType.JSON).get("/retrivestoredval/");
+        Assert.assertThat(response.getStatusCode(), Matchers.equalTo(200));
+        logger.info("RestAssuredIT::"+"Method Name :: testlistAllVarValue::Expected Status-HttpStatus.NO_CONTENT(200)::validation is Succesful");
     	}
-        catch (Exception e)
+    	catch (Exception e)
     	{
-    		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testcreateVarValue_positive::"+ e.getMessage());
+    		logger.error("Error in Class - "+"RestAssuredIT::"+"Method Name :: testlistAllVarValue::"+ e.getMessage());
+    	
     	}
     }
     
-    
+    //2.getVarValue - Negative
     @Test
-    public void testcreateVarValue_negative() {
+    public void testretreiveByVarName_negative() {
     	try {
-        Map<String,String> createvariable = new HashMap<>();
-        createvariable.put("storedValue", "xyx1111");
-        int statusCode=given().auth().basic("badusername","badpassword").contentType("application/json").body(createvariable).when().post("/api/storevalue/").then().statusCode(401).extract().response().statusCode();
-        Assert.assertEquals(statusCode, 200);
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response =  httpRequest.accept(ContentType.JSON).param("variablename","abc").get("/retrivestoredval/{variablename}");
+        Assert.assertThat(response.getStatusCode(), Matchers.equalTo(404));
+        logger.info("RestAssuredIT::"+"Method Name :: testretreiveByVarName_negative::HttpStatus.NOT_FOUND(404)::Nagative validation completed Succesfully");
     	}
-        catch (Exception e)
+    	catch (Exception e)
     	{
-    		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testcreateVarValue_negative::"+ e.getMessage());
+    		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testretreiveByVarName_negative::"+ e.getMessage());
     	}
     }
     
+    //3.getVarValue
     @Test
-    public void testcreateVarValue_negative_badauthentication() {  
+    public void testretreiveByVarName() {
     	try {
-    	 int statusCode=given().auth().basic("badusername","badpassword").delete().then().statusCode(401).extract().response().statusCode();
-    	 Assert.assertEquals(statusCode, 401);
+    	
+    	storedValues = new ArrayList<StoredValue>();
+        StoredValue first = new StoredValue("pqr", "2345");
+        this.saveVarValue(first);
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.accept(ContentType.JSON).param("variablename","pqr").get("/retrivestoredval/{variablename}");
+        Assert.assertThat(response.getStatusCode(), Matchers.equalTo(200));
+        logger.info("RestAssuredIT::"+"Method Name :: testretreiveByVarName::HttpStatus.OK(200)::validation completed Succesfully");
     	}
-    	 catch (Exception e)
-     	{
-     		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testcreateVarValue_negative_badauthentication::"+ e.getMessage());
-     	}
+    	catch (Exception e)
+    	{
+    		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testretreiveByVarName::"+ e.getMessage());
+    	}
     }
     
+  //4. createVarValue - Negative
+   @Test
+   public void testcreateVarValue_varExists()  throws JSONException,InterruptedException {
+   	try {
+   	   	 String apiBody = "{\"xyz\":\"2345\"}";
+   		 RequestSpecBuilder builder = new RequestSpecBuilder();
+   		 builder.setBody(apiBody);
+   		 builder.setContentType("application/json; charset=UTF-8");
+   		 RequestSpecification httpRequest = builder.build();
+   		 Response response = given().spec(httpRequest).when().post("/storevalue/");
+   		 Assert.assertThat(response.getStatusCode(), Matchers.equalTo(409));
+   	     logger.info("RestAssuredIT::"+"Method Name :: testcreateVarValue_varExists::HttpStatus.CONFLICT (409)::No new resource been Created");
+   	     
+ 	}
+     catch (Exception e)
+ 	{
+ 		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testcreateVarValue_varExists::"+ e.getMessage());
+ 	}
+ 
+   }
 
-	//DELETE
+  //  5.createVarValue
     @Test
-    public void testRetriveStoredValTestDeleteResource() {   	
-    try {
-    	 int statusCode=given().delete("/retrivestoredval/").then().statusCode(200).extract().response().statusCode();
-    	 Assert.assertEquals(statusCode, 200);
-    	}
-    	 catch (Exception e)
-     	{
-     		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testRetriveStoredValTestDeleteResource_nocontent::"+ e.getMessage());
-     	}
-    }
-    
-    @Test
-    public void testRetriveStoredValTestDeleteResource_nocontent() {
+    public void testcreateVarValue()  throws JSONException,InterruptedException {
     	try {
-    	 int statusCode=given().delete("/retrivestoredval/2").then().statusCode(204).extract().response().statusCode();
-    	 Assert.assertEquals(statusCode, 204);
-    	}
-    	 catch (Exception e)
-     	{
-     		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testRetriveStoredValTestDeleteResource_nocontent::"+ e.getMessage());
-     	}
+    		
+    	     String apiBody = "{\"lmn\":\"2345\"}";
+    	     RequestSpecBuilder builder = new RequestSpecBuilder();
+    	     builder.setBody(apiBody);
+    		 builder.setContentType("application/json; charset=UTF-8");
+    		 RequestSpecification  httpRequest = builder.build();
+    		 Response response = given().spec(httpRequest).when().post("/storevalue/");
+    		 Assert.assertThat(response.getStatusCode(), Matchers.equalTo(201));
+    	     logger.info("RestAssuredIT::"+"Method Name :: testcreateVarValue::HttpStatus.CREATED(201)::new resource has been Created");
+    		 
+  	}
+      catch (Exception e)
+  	{
+  		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testcreateVarValue::"+ e.getMessage());
+  	}
+  
     }
+    
+    //6. updateVarValue - Negative
     
     @Test
-    public void testRetriveStoredValTestDeleteResource_badauthentication() {   	
+    public void testupdateVarValue_negative()  throws JSONException,InterruptedException {
     	try {
-    	 int statusCode=given().auth().basic("badusername","badpassword").delete().then().statusCode(401).extract().response().statusCode();
-    	 Assert.assertEquals(statusCode, 401);
-    	}
-    	 catch (Exception e)
-     	{
-     		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testRetriveStoredValTestDeleteResource_badauthentication::"+ e.getMessage());
-     	}
+    		//case -1 Duplicate resource - varExists
+    		 String apiBody = "{\"jio\":\"2345\"}";
+    		 RequestSpecBuilder builder = new RequestSpecBuilder();
+    		 builder.setBody(apiBody);
+    		 builder.setContentType("application/json; charset=UTF-8");
+    		 RequestSpecification httpRequest = builder.build();
+    		 Response response = given().spec(httpRequest).param("variablename","rjk").when().put("storevalue/{variablename}");
+    		 Assert.assertThat(response.getStatusCode(), Matchers.equalTo(409));
+    	     logger.info("RestAssuredIT::"+"Method Name :: testupdateVarValue_negative::HttpStatus.NOT_FOUND (404)::No resource could be found for updating");
+    	     
+  	}
+      catch (Exception e)
+  	{
+  		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testupdateVarValue_negative::"+ e.getMessage());
+  	}
+  
     }
-    
-    //PUT
-    @Test
-    public void testupdateVarValue_positive() {   
-    	try {
-    	 Map<String,String> storedValue = new HashMap<>();
-    	 storedValue.put("variablename", "xyx1111");
-    	 int statusCode=given().contentType("application/json").pathParam("variablename", "test").body(storedValue).when().put("/storevalue/{variablename}").then().statusCode(201).extract().response().statusCode();	
-    	 Assert.assertEquals(statusCode, 201);
+
+   //7.updateVarValue
+     
+     @Test
+     public void testupdateVarValue()  throws JSONException,InterruptedException {
+     	try {
+     		
+     	     String apiBody = "{\"xyz\":\"2345\"}";
+     	     RequestSpecBuilder builder = new RequestSpecBuilder();
+     	     builder.setBody(apiBody);
+     		 builder.setContentType("application/json; charset=UTF-8");
+     		 RequestSpecification  httpRequest = builder.build();
+     		 Response response = given().spec(httpRequest).param("variablename","xyz").when().put("storevalue/{variablename}");
+     		 Assert.assertThat(response.getStatusCode(), Matchers.equalTo(200));
+     	     logger.info("RestAssuredIT::"+"Method Name :: testupdateVarValue::HttpStatus.OK(200)::resource has been Updated");
+     		 
+   	}
+       catch (Exception e)
+   	{
+   		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testupdateVarValue::"+ e.getMessage());
+   	}
+   
+     }
+     
+    //8.deleteAllVarsVals
+     
+     public void testdeleteAllVarsVals() {
+      	try {
+      		
+      		 RequestSpecification httpRequest = RestAssured.given();
+             Response response = httpRequest.accept(ContentType.JSON).delete("/retrivestoredval/");
+             Assert.assertThat(response.getStatusCode(), Matchers.equalTo(204));
+             logger.info("RestAssuredIT::"+"Method Name :: testdeleteAllVarsVals::HttpStatus.NO_CONTENT(204)::"+ "Validation is Succesful");
+      		 
     	}
-    	 catch (Exception e)
-     	{
-     		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testupdateVarValue_positive::"+ e.getMessage());
-     	}
-    }
-    
-    
-    @Test
-    public void testupdateVarValue_negative() { 
-    	try {
-    	 Map<String,String> storedValue = new HashMap<>();
-    	 storedValue.put("variablename", "");
-    	 int statusCode=given().contentType("application/json").pathParam("variablename", "test").body(storedValue).when().put("/storevalue/{variablename}").then().statusCode(404).extract().response().statusCode();	
-    	 Assert.assertEquals(statusCode, 404);
+        catch (Exception e)
+    	{
+    		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testdeleteAllVarsVals::"+ e.getMessage());
     	}
-    	 catch (Exception e)
-     	{
-     		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testupdateVarValue_negative::"+ e.getMessage());
-     	}
-    }
     
-    
-    @Test
-    public void testupdateVarValue_negative_badauthentication() {  
-    	try {
-    	 Map<String,String> storedValue = new HashMap<>();
-    	 storedValue.put("variablename", "xyx1111");
-    	 int statusCode=given().auth().basic("badusername","badpassword").pathParam("variablename", "variablename").contentType("application/json").body(storedValue).when().put("/storevalue/{variablename}").then().statusCode(401).extract().response().statusCode();	
-    	 Assert.assertEquals(statusCode, 401);
-    	}
-    	 catch (Exception e)
-     	{
-     		logger.info("Error in Class - "+"RestAssuredIT::"+"Method Name :: testupdateVarValue_negative_badauthentication::"+ e.getMessage());
-     	}
-    }
+      }
+     
+ 
     
 }
---
+++ src/test/java/org/cts/service/Original/SwaggerConfigTest.java
@@ -0,0 +1,8 @@
+package org.cts.service;
+
+/**
+ * Created by dharma on 3/20/18.
+ */
+public class SwaggerConfigTest {
+
+}
